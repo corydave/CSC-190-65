9a93506fd62a9a413383902ac35c38435fce3a5f
corydave <corydave@users.noreply.github.com>
2019-06-13 19:43:52 -0400

    Updated to 1.2.0

--- Shapes/ShapeManager.java
+++ Shapes/ShapeManager.java
@@ -2,20 +2,24 @@ public class ShapeManager {
     
     public static void main (String[] args) {
         
-        String version = "v1.0.0";
+        String version = "v1.2.0";
         System.out.println("SHAPE MANAGER! " + version);
         
         Square square01 = new Square();
         Square square02 = new Square(4);
         Square square03 = new Square(5, "MySquare");
         
-        System.out.println("The area of the square is " + square01.area());
-        System.out.println("The area of the square is " + square02.area());
-        // square03.name = "OVERRIDE";
-        System.out.println("The area of the square is " + square03.area() + " and the name is " + square03.getName());
-        square03.setName("OVERRIDE2");
-        square03.setSide(-1);
-        System.out.println("The area of the square is " + square03.area() + " and the name is " + square03.getName());
+        // System.out.println("The area of the square is " + square01.area());
+        // System.out.println("The area of the square is " + square02.area());
+        // // square03.name = "OVERRIDE";
+        // System.out.println("The area of the square is " + square03.area() + " and the name is " + square03.getName());
+        // square03.setName("OVERRIDE2");
+        // square03.setSide(-1);
+        // System.out.println("The area of the square is " + square03.area() + " and the name is " + square03.getName());
+        
+        System.out.println(square01);
+        System.out.println(square02);
+        System.out.println(square03);
     }
     
 }
